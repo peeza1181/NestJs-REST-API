7df04c60046c24198b093ac229e66c47b8021d9d
HelloReact <640101030029@dpu.ac.th>
2024-11-03 16:11:04 +0700

    add passport js & jwt module with nest.js

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController], // ควบคุมการทำงานของ Auth Module
-  providers: [AuthService], // ใช้เพื่อให้บริการที่ใช้ภายใน Auth Module
+  providers: [AuthService, JwtStrategy], // ใช้เพื่อให้บริการที่ใช้ภายใน Auth Module
 })
 export class AuthModule {}
