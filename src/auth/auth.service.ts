d59aafb095c8a8f0e21c0df278e40d289b74b145
HelloReact <640101030029@dpu.ac.th>
2024-11-03 14:23:09 +0700

    add Sign in logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -26,6 +26,7 @@ export class AuthService {
       });
 
       delete user.hash;
+
       // return the saved  user
       return user;
     } catch (error) {
@@ -33,15 +34,46 @@ export class AuthService {
         error instanceof
         PrismaClientKnownRequestError
       ) {
-        throw new ForbiddenException(
-          'Credentials taken',
-        );
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
       }
+      throw error;
     }
-    throw error;
   }
 
-  async signin() {
-    return { msg: 'I have signed in' };
+  async signin(dto: AuthDto) {
+    
+    // find the user by email
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+    // if user does not exist throw exception
+    if (!user)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+
+    // compare password
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+
+    // if password incorrect throw exception
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+
+    // send back the user
+    delete user.hash;
+    return user;
   }
 }
